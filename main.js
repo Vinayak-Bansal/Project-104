8f4129ca89b0b8d77a2fdef0e90c09b1c9e25e94
Vinayak-Bansal <97925994+Vinayak-Bansal@users.noreply.github.com>
2022-11-14 18:15:48 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+Webcam.set ({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+})
+camera=document.getElementById("camera")
+Webcam.attach('#camera')
+function takesnapshot() {
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML= '<img id="captured_image " src = " '+data_uri+'"/>'
+    })
+}
+classifier=ml5.imageClassifier("")
+model.json
